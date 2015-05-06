73456766fbd0e5a5a75cd8a32274361f99e00d64
ClassCat <support@classcat.com>
2015-05-06 14:34:57 +0900

    updated. (assets/cc-init.sh)

--- assets/cc-init.sh
+++ assets/cc-init.sh
@@ -2,13 +2,26 @@
 
 ########################################################################
 # ClassCat/Postfix Asset files
-# maintainer: Masashi Okumura < masao@classcat.com >
+# Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved.
 ########################################################################
 
-### HISTORY ###
+#--- HISTORY -----------------------------------------------------------
+# 06-may-15 : fixed.
+# 06-may-15 : Use HOSTNAME, DOMAINNAME and USERS.
 # 03-may-15 : Add sshd and code portion to handle root password.
 # 03-may-15 : Removed the nodaemon steps.
-#
+#-----------------------------------------------------------------------
+
+
+######################
+### INITIALIZATION ###
+######################
+
+function init () {
+  echo "ClassCat Info >> initialization code for ClassCat/Postfix"
+  echo "Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved."
+  echo ""
+}
 
 
 ############
@@ -16,8 +29,23 @@
 ############
 
 function change_root_password() {
-  echo -e "root:${password}" | chpasswd
-  # echo -e "${password}\n${password}" | passwd root
+  if [ -z "${ROOT_PASSWORD}" ]; then
+    echo "ClassCat Warning >> No ROOT_PASSWORD specified."
+  else
+    echo -e "root:${ROOT_PASSWORD}" | chpasswd
+    # echo -e "${password}\n${password}" | passwd root
+  fi
+}
+
+
+function put_public_key() {
+  if [ -z "$SSH_PUBLIC_KEY" ]; then
+    echo "ClassCat Warning >> No SSH_PUBLIC_KEY specified."
+  else
+    mkdir -p /root/.ssh
+    chmod 0700 /root/.ssh
+    echo "${SSH_PUBLIC_KEY}" > /root/.ssh/authorized_keys
+  fi
 }
 
 
@@ -26,10 +54,10 @@ function change_root_password() {
 ###############
 
 function proc_postfix_basic () {
-  echo "$domainname" > /etc/mailname
+  echo "${DOMAINNAME}" > /etc/mailname
 
-  postconf -e myhostname=$hostname
-  postconf -e mydestination=$domainname,$hostname,localhost.localdomain,localhost
+  postconf -e myhostname=${HOSTNAME}
+  postconf -e mydestination=${DOMAINNAME},${HOSTNAME},localhost.localdomain,localhost
 
   # enable a submission port. postconf should be used.
   sed -i -e "s/^#submission/submission/" /etc/postfix/master.cf 
@@ -38,18 +66,21 @@ function proc_postfix_basic () {
   postconf -F '*/*/chroot=n'
 }
 
+
 function add_accounts () {
-  echo $users | tr , \\n > /var/tmp/users
+  echo ${USERS} | tr , \\n > /var/tmp/users
   while IFS=':' read -r _user _id _pwd; do
     useradd -u $_id -s /usr/sbin/nologin $_user
-    echo -e "${_pwd}\n${_pwd}" | passwd $_user
+    echo -e "${_user}:${_pwd}" | chpasswd
+    #echo -e "${_pwd}\n${_pwd}" | passwd $_user
   done < /var/tmp/users
   rm /var/tmp/users
 }
 
+
 function proc_postfix_smtp_auth () {
   postconf -e smtpd_sasl_auth_enable=yes
-  postconf -e smtpd_sasl_local_domain=$domainname
+  postconf -e smtpd_sasl_local_domain=${DOMAINNAME}
   postconf -e broken_sasl_auth_clients=yes
   postconf -e smtpd_recipient_restrictions=permit_mynetworks,permit_sasl_authenticated,reject_unauth_destination
 
@@ -59,14 +90,15 @@ auxprop_plugin: sasldb
 mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
 EOF
 
-  echo $users | tr , \\n > /var/tmp/users
+  echo ${USERS} | tr , \\n > /var/tmp/users
   while IFS=':' read -r _user _id _pwd; do
-    echo $_pwd | saslpasswd2 -p -c -u $domainname $_user
+    echo $_pwd | saslpasswd2 -p -c -u ${DOMAINNAME} $_user
   done < /var/tmp/users
   rm /var/tmp/users
 
   chown postfix.sasl /etc/sasldb2
 }
+
 
 function proc_postfix () {
   proc_postfix_basic
@@ -87,7 +119,8 @@ function proc_spamassassin () {
   #sed -i -e "s/^CRON\s*=\s*0/CRON=1/" /etc/default/spamassassin
 
   # smtp       inet  n       -       n       -       -       smtpd
-  sed -i -e "s/^smtp\s*inet\s*n\s*\-\s*n\s*\-\s*\-\s*smtpd\s*/smtp       inet  n       -       n       -       -       smtpd -o content_filter=spamassassin/" /etc/postfix/master.cf
+  sed -ir "s/^smtp\s*inet\s*n\s*\-\s*n\s*\-\s*\-\s*smtpd\s*/smtp       inet  n       -       n       -       -       smtpd -o content_filter=spamassassin/" /etc/postfix/master.cf
+  # sed -i -e "s/^smtp\s*inet\s*n\s*\-\s*n\s*\-\s*\-\s*smtpd\s*/smtp       inet  n       -       n       -       -       smtpd -o content_filter=spamassassin/" /etc/postfix/master.cf
 
 
   echo 'spamassassin unix -     n       n       -       -       pipe' >> /etc/postfix/master.cf
@@ -102,10 +135,6 @@ function proc_spamassassin () {
 # See http://docs.docker.com/articles/using_supervisord/
 
 function proc_supervisor () {
-# removed the followings:
-#[supervisord]
-#nodaemon=true
-
   cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
 [program:spamassassin]
 command=/opt/cc-spamassassin.sh
@@ -117,7 +146,7 @@ command=/opt/cc-postfix.sh
 command=/usr/sbin/sshd -D
 
 [program:rsyslog]
-command=/usr/sbin/rsyslogd -n -c3
+command=/usr/sbin/rsyslogd -n
 EOF
 
   cat >> /opt/cc-spamassassin.sh <<EOF
@@ -138,7 +167,11 @@ EOF
 }
 
 
+### ENTRY POINT ###
+
+init
 change_root_password
+put_public_key
 proc_postfix
 proc_spamassassin
 proc_supervisor
