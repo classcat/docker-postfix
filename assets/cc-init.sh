fd371a2de637ed021b064587678eb73a7f08c68a
ClassCat <support@classcat.com>
2015-05-06 13:36:28 +0900

    init code added.

--- assets/cc-init.sh
+++ assets/cc-init.sh
@@ -2,13 +2,25 @@
 
 ########################################################################
 # ClassCat/Postfix Asset files
-# maintainer: Masashi Okumura < masao@classcat.com >
+# Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved.
 ########################################################################
 
-### HISTORY ###
+#--- HISTORY -----------------------------------------------------------
+# 06-may-15 : fixed.
 # 03-may-15 : Add sshd and code portion to handle root password.
 # 03-may-15 : Removed the nodaemon steps.
-#
+#-----------------------------------------------------------------------
+
+
+######################
+### INITIALIZATION ###
+######################
+
+function init () {
+  echo "ClassCat Info >> initialization code for ClassCat/Postfix"
+  echo "Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved."
+  echo ""
+}
 
 
 ############
@@ -16,8 +28,23 @@
 ############
 
 function change_root_password() {
-  echo -e "root:${password}" | chpasswd
-  # echo -e "${password}\n${password}" | passwd root
+  if [ -z "${ROOT_PASSWORD}" ]; then
+    echo "ClassCat Warning >> No ROOT_PASSWORD specified."
+  else
+    echo -e "root:${ROOT_PASSWORD}" | chpasswd
+    # echo -e "${password}\n${password}" | passwd root
+  fi
+}
+
+
+function put_public_key() {
+  if [ -z "$SSH_PUBLIC_KEY" ]; then
+    echo "ClassCat Warning >> No SSH_PUBLIC_KEY specified."
+  else
+    mkdir -p /root/.ssh
+    chmod 0700 /root/.ssh
+    echo "${SSH_PUBLIC_KEY}" > /root/.ssh/authorized_keys
+  fi
 }
 
 
@@ -38,14 +65,17 @@ function proc_postfix_basic () {
   postconf -F '*/*/chroot=n'
 }
 
+
 function add_accounts () {
   echo $users | tr , \\n > /var/tmp/users
   while IFS=':' read -r _user _id _pwd; do
     useradd -u $_id -s /usr/sbin/nologin $_user
-    echo -e "${_pwd}\n${_pwd}" | passwd $_user
+    echo -e "${_user}:${_pwd}" | chpasswd
+    #echo -e "${_pwd}\n${_pwd}" | passwd $_user
   done < /var/tmp/users
   rm /var/tmp/users
 }
+
 
 function proc_postfix_smtp_auth () {
   postconf -e smtpd_sasl_auth_enable=yes
@@ -68,6 +98,7 @@ EOF
   chown postfix.sasl /etc/sasldb2
 }
 
+
 function proc_postfix () {
   proc_postfix_basic
   add_accounts
@@ -87,7 +118,8 @@ function proc_spamassassin () {
   #sed -i -e "s/^CRON\s*=\s*0/CRON=1/" /etc/default/spamassassin
 
   # smtp       inet  n       -       n       -       -       smtpd
-  sed -i -e "s/^smtp\s*inet\s*n\s*\-\s*n\s*\-\s*\-\s*smtpd\s*/smtp       inet  n       -       n       -       -       smtpd -o content_filter=spamassassin/" /etc/postfix/master.cf
+  sed -ir "s/^smtp\s*inet\s*n\s*\-\s*n\s*\-\s*\-\s*smtpd\s*/smtp       inet  n       -       n       -       -       smtpd -o content_filter=spamassassin/" /etc/postfix/master.cf
+  # sed -i -e "s/^smtp\s*inet\s*n\s*\-\s*n\s*\-\s*\-\s*smtpd\s*/smtp       inet  n       -       n       -       -       smtpd -o content_filter=spamassassin/" /etc/postfix/master.cf
 
 
   echo 'spamassassin unix -     n       n       -       -       pipe' >> /etc/postfix/master.cf
@@ -102,10 +134,6 @@ function proc_spamassassin () {
 # See http://docs.docker.com/articles/using_supervisord/
 
 function proc_supervisor () {
-# removed the followings:
-#[supervisord]
-#nodaemon=true
-
   cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
 [program:spamassassin]
 command=/opt/cc-spamassassin.sh
@@ -117,7 +145,7 @@ command=/opt/cc-postfix.sh
 command=/usr/sbin/sshd -D
 
 [program:rsyslog]
-command=/usr/sbin/rsyslogd -n -c3
+command=/usr/sbin/rsyslogd -n
 EOF
 
   cat >> /opt/cc-spamassassin.sh <<EOF
@@ -138,7 +166,11 @@ EOF
 }
 
 
+### ENTRY POINT ###
+
+init
 change_root_password
+put_public_key
 proc_postfix
 proc_spamassassin
 proc_supervisor
